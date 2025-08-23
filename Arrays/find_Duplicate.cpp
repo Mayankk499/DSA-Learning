21bc31053efb1149c6130fad04d32dcde00dc900
Mayank Chauhan <124248806+Mayankk499@users.noreply.github.com>
2025-08-23 22:07:24 +0530

    Add function to find duplicate elements in an array

---
+++ Arrays/find_Duplicate.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findDuplicate(int arr[], int size){
+    int slow = arr[0], fast = arr[0];
+    
+    do {
+        slow = arr[slow];
+        fast = arr[arr[fast]];
+    } while (slow != fast);
+    
+    fast = arr[0];
+    while (slow != fast) {
+        slow = arr[slow];
+        fast = arr[fast];
+    }
+    cout<<"Duplicate Element : "<<slow;
+    
+    return slow;
+}
+int main(){
+    int arr[] = {1,2,3,4,1};
+    int size = 5;
+    findDuplicate(arr, 5);
+    return 0;
+}
